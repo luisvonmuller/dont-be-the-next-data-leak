6f582d8ece3ab93b95b6e7bcfec2911012977a12
Luís Von Muller <luis@vonmuller.com.br>
2021-11-17 19:04:48 -0300

    arrow func

--- src/Modules/Cypher/index.ts
+++ src/Modules/Cypher/index.ts
@@ -1,7 +1,7 @@
 import { CypherField } from './interface';
 import crypto from 'crypto';
 
-function cypherField<T>({ pseudoPublicKey, ownSecret, value }: CypherField<T>): string {
+const cypherField = <T>({ pseudoPublicKey, ownSecret, value }: CypherField<T>): string => {
   let key = ownSecret.substring(0, 32)
   const encrypter: any = crypto.createCipheriv('aes-256-ctr', Buffer.from(key), pseudoPublicKey);
   return encrypter.update(value, "utf-8", "hex");
