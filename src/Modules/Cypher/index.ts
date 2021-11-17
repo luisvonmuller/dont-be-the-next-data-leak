8c07e32587e26579833a4893eed43291910ee077
Luís Von Muller <luis@vonmuller.com.br>
2021-11-17 13:15:57 -0300

    Removed and improved something

--- src/Modules/Cypher/index.ts
+++ src/Modules/Cypher/index.ts
@@ -2,7 +2,7 @@ import { CypherField } from './interface';
 import crypto from 'crypto';
 
 function cypherField<T>({ pseudoPublicKey, ownSecret, value }: CypherField<T>): string {
-  let key = crypto.createHash('sha256').update(String(ownSecret)).digest('base64').substr(0, 32);
+  let key = ownSecret.substring(0, 32)
   const encrypter: any = crypto.createCipheriv('aes-256-ctr', Buffer.from(key), pseudoPublicKey);
   return encrypter.update(value, "utf-8", "hex");
 }
